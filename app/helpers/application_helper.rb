2f947b586dd6e9aeff91cd6bb91c69de29ac2d25
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-08-21 21:29:20 -0700

    modified communities display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,8 @@ module ApplicationHelper
     inline_svg_tag(filename, aria: true, nocomment: true, title: title, class: styles)
   end
 
+  def author_of?(record)
+    user_signed_in? && current_user.id == record.user_id
+  end
 
 end
